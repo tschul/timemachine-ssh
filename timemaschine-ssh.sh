d5d2968be7926c9bd8f73d04205b17273392de9b
tschul <tschul@users.noreply.github.com>
2021-04-09 11:41:27 +0200

    keep tunnel alive client-side for like 40 hours
    did not work any other way for me, raspberry pi Ubuntu / Mac OS Catalina

--- timemaschine-ssh.sh
+++ timemaschine-ssh.sh
@@ -37,7 +37,7 @@ createTunnel() {
 
     # Create tunnel to port 548 on remote host and make it avaliable at port $LOCAL_AFP_PORT at localhost
     # Also tunnel ssh for connection testing purposes
-    ssh -gNf -L "$LOCAL_AFP_PORT:$AFP_HOST:548" -C $REMOTE_LOGIN
+    ssh -o TCPKeepAlive=yes -o ServerAliveCountMax=9999 -o ServerAliveInterval=15 -gNf -L "$LOCAL_AFP_PORT:$AFP_HOST:548" -C $REMOTE_LOGIN
 
     if [[ $? -eq 0 ]]; then
         # Register AFP as service via dns-sd
